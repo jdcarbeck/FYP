e41f386560ead32f0123fb35540fd5f75443ff19
jdcarbeck <jdcarbeck@gmail.com>
2020-02-19 14:48:31 +0000

    create model added

---
+++ ModelGen/Model.py
@@ -0,0 +1,5 @@
+import gensim
+
+class Model:
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from ModelGen.ConceptExtract import Concepts
 from ModelGen.Corpus import Corpus
-
-"""Takes wiki dump and creates a LDA model from dump "Concepts" """
-def model_from_wiki():
-    pass
 
 corp = Corpus('./WikiCorpus/WaterGateText/AA/wiki_00')
 print(len(corp.get_concepts()))
